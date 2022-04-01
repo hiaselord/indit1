45d95052263610b31a7b3f991c0dcf59e484d0e9
hiaselord <102793478+hiaselord@users.noreply.github.com>
2022-04-01 11:14:11 +0200

    Add files via upload

---
+++ Party_upper_lower.py
@@ -0,0 +1,13 @@
+# Party upper/lower
+
+wetter = input("Wetterprognosen 'Regen' oder 'Sonne' eingeben: ")
+
+tag=wetter.upper
+
+if tag() =="REGEN":
+    print("Kellerparty")
+
+elif tag() =="SONNE":
+    print("Gartenparty")
+else:
+    print("Bitte 'Regen' oder 'Sonne' eingeben:")
