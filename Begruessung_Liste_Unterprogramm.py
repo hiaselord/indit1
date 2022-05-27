fae0749c662fed8210ad2a3e6a1ca1b227f68f78
hiaselord <102793478+hiaselord@users.noreply.github.com>
2022-05-27 14:18:24 +0200

    Add files via upload

---
+++ Begruessung_Liste_Unterprogramm.py
@@ -0,0 +1,9 @@
+name = ["Peter", "Dora", "Kevin", "Fritz", "Sarah"]
+
+def begruessung(name):
+    print("Hallo", name)
+    print("Schön das du da bist")
+    print("Viel Spaß mit dem Programm!")
+
+for name in name:
+    begruessung(name)
